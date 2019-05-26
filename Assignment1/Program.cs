e6e8b7db1b860899d78546930f239d1d721f981e
Sukhchain Kharoud <sukhchainkharoud7@gmail.com>
2019-05-26 15:38:52 -0400

    <Software Quality Assignment 1>

---
+++ Assignment1/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Go();
+        }
+        public void Go()
+        {
+            Rectangle obj = new Rectangle();
+           
+            bool exitProgram = false;
+
+
+            string len = string.Empty;
+            int length = 0;
+            do
+            {
+                Console.Write("Enter the Length of Rectangle ");
+                len = Console.ReadLine();
+            } while (!(ValidateInt(len)));
+            length = int.Parse(len);
+            obj.SetLength(length);
+
+
+
+
+
+            string widt = string.Empty;
+            int width = 0;
+            do
+            {
+                Console.Write("Enter the Width of Rectangle ");
+                widt = Console.ReadLine();
+            } while (!(ValidateInt(widt)));
+            width = int.Parse(widt);
+            obj.SetWidth(width);
+
+            Rectangle secondConstructor = new Rectangle(length, width);
+
+
+
+
+
+            do
+            { 
+               
+
+                
+               
+                    ArrayList Schedule = new ArrayList();
+
+                    Schedule.Add(":  Get Rectangle Length");
+                    Schedule.Add(":  Change Rectangle Length");
+                    Schedule.Add(":  Get Rectangle Width");
+                    Schedule.Add(":  Change Rectangle Width");
+                    Schedule.Add(":  Get Rectangle Perimeter");
+                    Schedule.Add(":  Get Rectangle Area");
+                    Schedule.Add(":  Exit");
+
+
+
+                    for (int i = 0; i < Schedule.Count; i++)
+                    {
+                        Console.WriteLine("{0}. {1}", i + 1, Schedule[i]);
+                    }
+                
+
+                
+
+                string menuSelected = string.Empty;
+                do
+                {
+                    menuSelected = Console.ReadLine();
+                } while (!ValidateInt(menuSelected));
+
+                int menuOption = int.Parse(menuSelected);
+
+                switch (menuOption)
+                {
+                    case 1:
+                        Console.WriteLine("Length is :{0}", obj.GetLength());
+                        break;
+                    case 2:
+                        string leng = string.Empty;
+                        int lengthl = 0;
+                        do
+                        {
+                            Console.Write("Enter New Length : ");
+                            leng = Console.ReadLine();
+                        } while (!(ValidateInt(leng)));
+                        lengthl = int.Parse(leng);
+                        Console.WriteLine("New Length is :{0}",obj.SetLength(lengthl));
+                        break;
+                    case 3:
+                        Console.WriteLine("Width is :{0}", obj.GetWidth());
+                        break;
+                    case 4:
+                        string wid = string.Empty;
+                        int widthw = 0;
+                        do
+                        {
+                            Console.Write("Enter New Width : ");
+                            wid = Console.ReadLine();
+                        } while (!(ValidateInt(wid)));
+                        widthw = int.Parse(wid);
+                        Console.WriteLine("New Width is :{0}", obj.SetWidth(widthw));
+                        break;
+                    case 5:
+                        Console.WriteLine("Perimeter is :{0}", obj.GetPerimeter());
+                        break;
+                    case 6:
+                        Console.WriteLine("Area is  :{0}", obj.GetArea());
+                        break;
+                    case 7:
+                        exitProgram = true;
+                        break;
+                    
+                    default:
+                        Console.WriteLine("Please, Select Valid Input");
+                        break;
+                }
+
+
+
+
+
+
+                } while (!exitProgram) ;
+
+
+
+
+            
+
+}
+
+
+
+        bool ValidateInt(string userinput)
+        {
+            int integer = 0;
+
+            if (int.TryParse(userinput, out integer))
+            {
+                if (integer > 0)
+                {
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+
+    
+}
---
+++ Assignment1_Tests/Assignment1_Tests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment1;
+using NUnit.Framework;
+
+namespace Assignment1_Tests
+{
+    [TestFixture]
+    class Assignment1_Tests
+    {
+
+        [Test]
+        public void GetLenght_input3_expectLengthEquals3()
+        {
+            //Arrange
+            int l = 3;
+            int w = 4;
+            Rectangle testRectangle = new Rectangle(l, w);
+
+            //Act
+            int length = testRectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(length,l);
+
+        }
+
+        [Test]
+        public void GetLenght_input0_expectLengthEquals0()
+        {
+            //Arrange
+            int l = 0;
+            int w = 0;
+            Rectangle testRectangle = new Rectangle(l, w);
+
+            //Act
+            int length = testRectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(length, l);
+
+        }
+
+        [Test]
+        public void GetLenght_input999999999_expectLengthEquals999999999()
+        {
+            //Arrange
+            int l = 999999999;
+            int w = 999999999;
+            Rectangle testRectangle = new Rectangle(l, w);
+
+            //Act
+            int length = testRectangle.GetLength();
+
+            //Assert
+            Assert.AreEqual(length, l);
+
+        }
+
+
+
+
+        [Test]
+        public void GetWidth_input3_expectLengthEquals3()
+        {
+            //Arrange
+            int l = 3;
+            int w = 4;
+            Rectangle testRectangle = new Rectangle(l, w);
+
+            //Act
+            int width = testRectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(width, w);
+
+        }
+
+        [Test]
+        public void GetWidth_input0_expectLengthEquals0()
+        {
+            //Arrange
+            int l = 0;
+            int w = 0;
+            Rectangle testRectangle = new Rectangle(l, w);
+
+            //Act
+            int width = testRectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(width, w);
+
+        }
+
+        [Test]
+        public void GetWidth_input999999999_expectLengthEquals999999999()
+        {
+            //Arrange
+            int l = 999999999;
+            int w = 999999999;
+            Rectangle testRectangle = new Rectangle(l, w);
+
+            //Act
+            int width = testRectangle.GetWidth();
+
+            //Assert
+            Assert.AreEqual(width, w);
+
+        }
+
+        [Test]
+        public void SetLenght_input12_expectLengthEquals12()
+        {
+            //Arrange
+            int l = 12;
+            int w = 0;
+            Rectangle testRectangle = new Rectangle(l, w);
+
+            //Act
+            int length = testRectangle.SetLength(l);
+
+            //Assert
+            Assert.AreEqual(length, l);
+
+        }
+
+
+
+
+    }
+}
